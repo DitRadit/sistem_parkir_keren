4042c023dcb2b46ba71cbbe0a0e982fec287465d
yuandaffa <yuandaffa23@gmail.com>
2026-05-28 16:56:10 +0700

    insert util

--- src/java/util/MidtransUtil.java
+++ src/java/util/MidtransUtil.java
@@ -4,10 +4,18 @@
  */
 package util;
 
-/**
- *
- * @author asus
- */
+import com.midtrans.Config;
+
 public class MidtransUtil {
-    
+
+    public static void init() {
+
+        Config.serverKey =
+                "SB-Mid-server-XXXXXXXXXXXXXXXX";
+
+        Config.clientKey =
+                "SB-Mid-client-XXXXXXXXXXXXXXXX";
+
+        Config.isProduction = false;
+    }
 }
--- src/java/util/QRUtil.java
+++ src/java/util/QRUtil.java
@@ -4,10 +4,37 @@
  */
 package util;
 
-/**
- *
- * @author asus
- */
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class QRUtil {
-    
+
+    public static void generateQRCode(
+            String text,
+            String path)
+            throws Exception {
+
+        BitMatrix matrix =
+                new MultiFormatWriter().encode(
+                        text,
+                        BarcodeFormat.QR_CODE,
+                        300,
+                        300
+                );
+
+        Path qrPath =
+                FileSystems.getDefault()
+                        .getPath(path);
+
+        MatrixToImageWriter.writeToPath(
+                matrix,
+                "PNG",
+                qrPath
+        );
+    }
 }
