83f83e91d6ffd645b5b154272be63eee76875ac0
DitRadit <adiradityagt@gmail.com>
2026-05-28 17:01:24 +0700

    update midtras key and client

--- src/java/util/MidtransUtil.java
+++ src/java/util/MidtransUtil.java
@@ -1,21 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package util;
 
-import com.midtrans.Config;
+import com.midtrans.Midtrans;
 
 public class MidtransUtil {
 
     public static void init() {
 
-        Config.serverKey =
-                "SB-Mid-server-XXXXXXXXXXXXXXXX";
-
-        Config.clientKey =
-                "SB-Mid-client-XXXXXXXXXXXXXXXX";
-
-        Config.isProduction = false;
+        Midtrans.serverKey = "SB-Mid-server-a0U-8yP3Wgcur4twm-zwxYBQ";
+        Midtrans.clientKey = "SB-Mid-client-ONI-X_ehI3onqvpY";
+        Midtrans.isProduction = false;
     }
 }
